aa076ec1bfbadbe750c345c69c8a27f0d9bedc5e
Igor Tiulkanov <itiulkanov@infoblox.com>
2022-11-18 22:07:47 +0300

    Minor refactoring

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 from torch import optim
+from torch import cuda
 from tqdm.auto import tqdm
-from helper import *
 from model.generator import SkipEncoderDecoder, input_noise
+from helper import *
 
-def remove_watermark(image_path, mask_path, max_dim, reg_noise, input_depth, lr, show_step, training_steps, tqdm_length = 100):
+
+def remove_watermark(image_path, mask_path, max_dim, reg_noise, input_depth, lr, show_step, training_steps,
+                     tqdm_length=100):
+    import torch
     DTYPE = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
     if not torch.cuda.is_available():
         print('\nSetting device to "cpu", since torch is not built with "cuda" support...')
@@ -12,11 +16,12 @@ def remove_watermark(image_path, mask_path, max_dim, reg_noise, input_depth, lr,
     image_np, mask_np = preprocess_images(image_path, mask_path, max_dim)
 
     print('Building the model...')
+    get_less_used_gpu()
     generator = SkipEncoderDecoder(
         input_depth,
-        num_channels_down = [128] * 5,
-        num_channels_up = [128] * 5,
-        num_channels_skip = [128] * 5
+        num_channels_down=[128] * 5,
+        num_channels_up=[128] * 5,
+        num_channels_skip=[128] * 5
     ).type(DTYPE)
 
     objective = torch.nn.MSELoss().type(DTYPE)
@@ -32,7 +37,7 @@ def remove_watermark(image_path, mask_path, max_dim, reg_noise, input_depth, lr,
 
     print('\nStarting training...\n')
 
-    progress_bar = tqdm(range(training_steps), desc = 'Completed', ncols = tqdm_length)
+    progress_bar = tqdm(range(training_steps), desc='Completed', ncols=tqdm_length)
 
     for step in progress_bar:
         optimizer.zero_grad()
@@ -40,22 +45,22 @@ def remove_watermark(image_path, mask_path, max_dim, reg_noise, input_depth, lr,
 
         if reg_noise > 0:
             generator_input = generator_input_saved + (noise.normal_() * reg_noise)
-            
+
         output = generator(generator_input)
-    
+
         loss = objective(output * mask_var, image_var * mask_var)
         loss.backward()
 
         if step % show_step == 0:
             output_image = torch_to_np_array(output)
-            visualize_sample(image_np, output_image, nrow = 2, size_factor = 10)
-        
-        progress_bar.set_postfix(Loss = loss.item())
-            
+            visualize_sample(image_np, output_image, nrow=2, size_factor=10)
+
+        progress_bar.set_postfix(Loss=loss.item())
+
         optimizer.step()
-    
+
     output_image = torch_to_np_array(output)
-    visualize_sample(output_image, nrow = 1, size_factor = 10)
+    visualize_sample(output_image, nrow=1, size_factor=10)
 
     pil_image = Image.fromarray((output_image.transpose(1, 2, 0) * 255.0).astype('uint8'))
 
@@ -63,3 +68,61 @@ def remove_watermark(image_path, mask_path, max_dim, reg_noise, input_depth, lr,
     print(f'\nSaving final output image to: "{output_path}"\n')
 
     pil_image.save(output_path)
+
+
+def get_less_used_gpu(gpus=None, debug=True):
+    """Inspect cached/reserved and allocated memory on specified gpus and return the id of the less used device"""
+    if gpus is None:
+        warn = 'Falling back to default: all gpus'
+        gpus = range(cuda.device_count())
+    elif isinstance(gpus, str):
+        gpus = [int(el) for el in gpus.split(',')]
+
+    # check gpus arg VS available gpus
+    sys_gpus = list(range(cuda.device_count()))
+    if len(gpus) > len(sys_gpus):
+        gpus = sys_gpus
+        warn = f'WARNING: Specified {len(gpus)} gpus, but only {cuda.device_count()} available. Falling back to default: all gpus.\nIDs:\t{list(gpus)}'
+    elif set(gpus).difference(sys_gpus):
+        # take correctly specified and add as much bad specifications as unused system gpus
+        available_gpus = set(gpus).intersection(sys_gpus)
+        unavailable_gpus = set(gpus).difference(sys_gpus)
+        unused_gpus = set(sys_gpus).difference(gpus)
+        gpus = list(available_gpus) + list(unused_gpus)[:len(unavailable_gpus)]
+        warn = f'GPU ids {unavailable_gpus} not available. Falling back to {len(gpus)} device(s).\nIDs:\t{list(gpus)}'
+
+    cur_allocated_mem = {}
+    cur_cached_mem = {}
+    max_allocated_mem = {}
+    max_cached_mem = {}
+    for i in gpus:
+        cur_allocated_mem[i] = cuda.memory_allocated(i)
+        cur_cached_mem[i] = cuda.memory_reserved(i)
+        max_allocated_mem[i] = cuda.max_memory_allocated(i)
+        max_cached_mem[i] = cuda.max_memory_reserved(i)
+    min_allocated = min(cur_allocated_mem, key=cur_allocated_mem.get)
+    if debug:
+        print(warn)
+        print('Current allocated memory:', {f'cuda:{k}': v for k, v in cur_allocated_mem.items()})
+        print('Current reserved memory:', {f'cuda:{k}': v for k, v in cur_cached_mem.items()})
+        print('Maximum allocated memory:', {f'cuda:{k}': v for k, v in max_allocated_mem.items()})
+        print('Maximum reserved memory:', {f'cuda:{k}': v for k, v in max_cached_mem.items()})
+        print('Suggested GPU:', min_allocated)
+    return min_allocated
+
+
+def free_memory(to_delete: list, debug=False):
+    import gc
+    import inspect
+    calling_namespace = inspect.currentframe().f_back
+    if debug:
+        print('Before:')
+        get_less_used_gpu(debug=True)
+
+    for _var in to_delete:
+        calling_namespace.f_locals.pop(_var, None)
+        gc.collect()
+        cuda.empty_cache()
+    if debug:
+        print('After:')
+        get_less_used_gpu(debug=True)
--- helper.py
+++ helper.py
@@ -5,42 +5,51 @@ import matplotlib.pyplot as plt
 import torch
 from torchvision.utils import make_grid
 
+
 def pil_to_np_array(pil_image):
     ar = np.array(pil_image)
     if len(ar.shape) == 3:
-        ar = ar.transpose(2,0,1)
+        ar = ar.transpose(2, 0, 1)
     else:
         ar = ar[None, ...]
     return ar.astype(np.float32) / 255.
 
+
 def np_to_torch_array(np_array):
     return torch.from_numpy(np_array)[None, :]
+
 
 def torch_to_np_array(torch_array):
     return torch_array.detach().cpu().numpy()[0]
 
-def read_image(path, image_size = -1):
+
+def read_image(path):
     pil_image = Image.open(path)
     return pil_image
 
-def crop_image(image, crop_factor = 64):
+
+def crop_image(image, crop_factor=64):
     shape = (image.size[0] - image.size[0] % crop_factor, image.size[1] - image.size[1] % crop_factor)
-    bbox = [int((image.shape[0] - shape[0])/2), int((image.shape[1] - shape[1])/2), int((image.shape[0] + shape[0])/2), int((image.shape[1] + shape[1])/2)]
+    bbox = [int((image.shape[0] - shape[0]) / 2), int((image.shape[1] - shape[1]) / 2),
+            int((image.shape[0] + shape[0]) / 2), int((image.shape[1] + shape[1]) / 2)]
     return image.crop(bbox)
 
-def get_image_grid(images, nrow = 3):
+
+def get_image_grid(images, nrow=3):
     torch_images = [torch.from_numpy(x) for x in images]
     grid = make_grid(torch_images, nrow)
     return grid.numpy()
-    
-def visualize_sample(*images_np, nrow = 3, size_factor = 10):
+
+
+def visualize_sample(*images_np, nrow=3, size_factor=10):
     c = max(x.shape[0] for x in images_np)
-    images_np = [x if (x.shape[0] == c) else np.concatenate([x, x, x], axis = 0) for x in images_np]
+    images_np = [x if (x.shape[0] == c) else np.concatenate([x, x, x], axis=0) for x in images_np]
     grid = get_image_grid(images_np, nrow)
-    plt.figure(figsize = (len(images_np) + size_factor, 12 + size_factor))
+    plt.figure(figsize=(len(images_np) + size_factor, 12 + size_factor))
     plt.axis('off')
     plt.imshow(grid.transpose(1, 2, 0))
     plt.show()
+
 
 def max_dimension_resize(image_pil, mask_pil, max_dim):
     w, h = image_pil.size
@@ -53,6 +62,7 @@ def max_dimension_resize(image_pil, mask_pil, max_dim):
         h = max_dim
     return image_pil.resize((w, h)), mask_pil.resize((w, h))
 
+
 def preprocess_images(image_path, mask_path, max_dim):
     image_pil = read_image(image_path).convert('RGB')
     mask_pil = read_image(mask_path).convert('RGB')
@@ -64,6 +74,6 @@ def preprocess_images(image_path, mask_path, max_dim):
 
     print('Visualizing mask overlap...')
 
-    visualize_sample(image_np, mask_np, image_np * mask_np, nrow = 3, size_factor = 10)
+    visualize_sample(image_np, mask_np, image_np * mask_np, nrow=3, size_factor=10)
 
     return image_np, mask_np
